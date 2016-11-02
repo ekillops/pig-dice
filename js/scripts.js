b7a73fd0e95e2dce949f8cdeb879afd7024f9850
Erik Killops and Josh Rinard <erik@>
2016-11-02 14:00:39 -0700

    adds second player functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
 /* Back End */
 
+//Player constructor
+function Player(playerName, playerNumber, score) {
+  this.playerName = playerName;
+  this.playerNumber = playerNumber;
+  this.score = score;
+}
+
+//Player prototype method to score points
+Player.prototype.scorePoints = function(points) {
+  this.score += points;
+};
+
+//Method to reset score on new game
+Player.prototype.resetScore = function() {
+  this.score = 0;
+};
+
+//Function to add to the player index counter for turn alternation
+function switchPlayer(currentPlayerNumber, numberOfPlayers) {
+  numberOfPlayers -= 1;
+  var nextPlayer = 0;
+
+  if (currentPlayerNumber === numberOfPlayers) {
+    nextPlayer = 0;
+  } else {
+    nextPlayer = currentPlayerNumber + 1;
+  }
+  return nextPlayer;
+}
 
 //Taken from stackoverflow: generate a random integer
 function getRandomInt(min, max) {
@@ -12,24 +41,32 @@ function rollD6() {
   return roll;
 }
 
-function pigRoll(){
-
-  if (pigRoll > 1) {}
-};
-
 
 /* Front End */
 
 
 
 $(document).ready(function(){
+  //Create player objects
+  var player1 = new Player("Player 1", 1, 0);
+  var player2 = new Player("Player 2", 2, 0);
 
-  //Initial variables to hold turn score and player score
-  var playerTotal = 0;
+  //Create array to hold player objects
+  var players = [player1, player2];
 
+  //Variable to hold number of players
+  var numberOfPlayers = 2;
+
+  //Initial variables player scores
+  var playerTotal1 = 0;
+  var playerTotal2 = 0;
+
+  //Variables to track current player and turn score
+  var currentPlayerIndex = 0;
   var turnTotal = 0;
 
-
+  //Display starting active player
+  $("#activePlayer").text(players[currentPlayerIndex].playerName);
 
 /* Roll button click */
   $("button#roll").click(function(){
@@ -42,8 +79,21 @@ $(document).ready(function(){
 
     //Add dieRoll to turnTotal unless a 1 is rolled
     if (dieRoll != 1) {
+      //Add roll to turn total
       turnTotal += dieRoll;
+
     } else {
+
+      //Display result to player1Total or player2Total span
+      $("#player" + players[currentPlayerIndex].playerNumber + "Total").text(players[currentPlayerIndex].score);
+
+      //Switch to next player's turn
+      currentPlayerIndex = switchPlayer(currentPlayerIndex, numberOfPlayers);
+
+      //Display new current player
+      $("#activePlayer").text(players[currentPlayerIndex].playerName);
+
+      //Reset turn total
       turnTotal = 0;
     }
 
@@ -56,15 +106,12 @@ $(document).ready(function(){
   $("button#hold").click(function(){
 
     //Add turn total to player score
-    playerTotal += turnTotal;
+    players[currentPlayerIndex].scorePoints(turnTotal);
 
-    //Display result to playerTotal span
-    $("#playerTotal").text(playerTotal);
-
-    //Reset turnTotal
-    turnTotal = 0;
-
-    if (playerTotal > 99) {
+    //Display result to player1Total or player2Total span
+    $("#player" + players[currentPlayerIndex].playerNumber + "Total").text(players[currentPlayerIndex].score);
+    // check for win condition
+    if (players[currentPlayerIndex].score > 99) {
       //Display end game message
       $("#gameEnd").show();
     } else {
@@ -72,17 +119,29 @@ $(document).ready(function(){
       $("#dieRoll, #turnTotal").empty();
     }
 
+    //Switch to next player's turn
+    currentPlayerIndex = switchPlayer(currentPlayerIndex, numberOfPlayers);
+    //Display new current player
+    $("#activePlayer").text(players[currentPlayerIndex].playerName);
+
+    //Reset turnTotal
+    turnTotal = 0;
+
+
+
   });
 
 /*  Reset button */
 
   $("button#reset").click(function(){
-    //assigns player total to 0
-    playerTotal = 0;
+    //Reset player scores to 0
+    players.forEach(function(player){
+      player.resetScore();
+    });
     //assigns turn total to 0
     turnTotal = 0;
     //clears values in display fields
-    $("#dieRoll, #turnTotal, #playerTotal").empty();
+    $("#dieRoll, #turnTotal, #player1Total, #player2Total").empty();
 
   });
 
